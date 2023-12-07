71e5b00914f32dffd734fa1995336670d1be98f0
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2023-12-07 21:37:58 +0500

    Homework done

---
+++ index.js
@@ -0,0 +1,40 @@
+    // 1-chi masala
+// let a = +prompt("Birinchi sonni kiriting...");
+// let b = +prompt("Ikkinchi sonni kiriting...");
+// let c = +prompt("Uchinchi sonni kiriting...");
+// let res = (a + b + c);
+
+// console.log(res);
+
+
+    // 2-chi masala
+// let a = +prompt(`Birinchi sonni kiriting...`);
+// let b = +prompt(`Ikkinchi sonni kiriting...`);
+// let res = ((a * a) + (b * b));
+
+// console.log(res);
+
+
+    // 3-chi masala
+// let a = +prompt('Boyini kiriting...');
+// let b = +prompt('Enini kiriting...');
+// let P = (2 * (a + b));
+
+// console.log(P);
+
+
+    // 4-chi masala
+/* let a = +prompt("tomonini kiriting...");
+let V = (a * a * a);
+let S = (6 * a * a);
+
+console.log(V);
+console.log(S); */
+
+
+    // 5-chi masala
+/* let d = +prompt(`diametrni kiriting`);
+let pi = 3.14;
+let S = (pi * (d/2) * (d/2));
+
+console.log(S);  */
